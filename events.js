18f0e9231423638dd31e6d4897f4ae0e76087559
Ionut Moraru <morion4000@gmail.com>
2015-01-21 19:04:14 +0200

    First version of the framework

---
+++ events.js
@@ -0,0 +1,36 @@
+(function () {
+    var topics = {};
+
+    var _subscribe = function(topic, listener) {
+        if (!topics[topic]) {
+            topics[topic] = {
+                queue: []
+            }
+        }
+        
+        topics[topic].queue.push(listener);
+    }
+
+    var _unsubscribe = function(index) {
+        delete topics[topic].queue[index];
+    }
+
+    var _publish = function(topic, message) {
+        var message = message || {};
+
+        if (!topics[topic] || !topics[topic].queue.length) {
+            return;
+        }
+
+        topics[topic].queue.forEach(function(listener) {
+            listener(message);
+        });
+    }
+
+    window.events = {
+        subscribe: _subscribe,
+        unsubscribe: _unsubscribe,
+        publish: _publish,
+        topics: topics
+    };
+})();
