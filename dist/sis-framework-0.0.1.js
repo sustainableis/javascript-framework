3fd798fcef8fdc76f4d2b5ba641281ad5951b224
Ionut Moraru <morion4000@gmail.com>
2015-01-24 11:29:27 +0200

    Updated dist files

--- dist/sis-framework-0.0.1.js
+++ dist/sis-framework-0.0.1.js
@@ -512,37 +512,6 @@
     ]);
 
     /**
-     * Resource for retrieving Facilties
-     *
-     * @param {number|string} id
-     * @param {string} controller
-     * @param {string} verb
-     * @param {string} action
-     *
-     * Endpoints example:
-     *  - /v1/facilities
-     *  - /v1/facilities/1
-     *  - /v1/facilities/1/outputs
-     *  - /v1/facilities?organization_id=1
-     *  - /v1/facilities/outputs/types
-     *  - /v1/facilities/1/outputs/tree/validate
-     *  - /v1/facilities/1/wms/products
-     */
-    angular.module('sis.api').factory('FacilitiesService', [
-        '$resource',
-        'url',
-        'version',
-        function($resource, url, version) {
-            return $resource(url + version + '/facilities/:id/:controller/:verb/:action', {
-                id: '@id',
-                controller: '@controller',
-                verb: '@verb',
-                action: '@action'
-            });
-        }
-    ]);
-
-    /**
      * Resource for retrieving Organizations
      *
      * @param {number|string} id
@@ -703,3 +672,38 @@
     ]);
 // TODO: Inject underscore.js and jQuery as dependencies or remove references to them
 })(window, window.angular, window.events);
+
+'use strict';
+
+(function(angular) {
+    /**
+     * Resource for retrieving Facilties
+     *
+     * @param {number|string} id
+     * @param {string} controller
+     * @param {string} verb
+     * @param {string} action
+     *
+     * Endpoints example:
+     *  - /v1/facilities
+     *  - /v1/facilities/1
+     *  - /v1/facilities/1/outputs
+     *  - /v1/facilities?organization_id=1
+     *  - /v1/facilities/outputs/types
+     *  - /v1/facilities/1/outputs/tree/validate
+     *  - /v1/facilities/1/wms/products
+     */
+    angular.module('sis.api').factory('FacilitiesService', [
+        '$resource',
+        'url',
+        'version',
+        function($resource, url, version) {
+            return $resource(url + version + '/facilities/:id/:controller/:verb/:action', {
+                id: '@id',
+                controller: '@controller',
+                verb: '@verb',
+                action: '@action'
+            });
+        }
+    ]);
+})(window.angular);
