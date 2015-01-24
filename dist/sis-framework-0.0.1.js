b05cce6f11dc23e9c8ccba95d592dd3ed2127631
Ionut Moraru <morion4000@gmail.com>
2015-01-24 10:08:58 +0200

    Dist files

--- dist/sis-framework-0.0.1.js
+++ dist/sis-framework-0.0.1.js
@@ -1,5 +1,44 @@
 'use strict';
 
+(function () {
+    var topics = {};
+
+    var _subscribe = function(topic, listener) {
+        if (!topics[topic]) {
+            topics[topic] = {
+                queue: []
+            }
+        }
+        
+        topics[topic].queue.push(listener);
+    }
+
+    var _unsubscribe = function(index) {
+        delete topics[topic].queue[index];
+    }
+
+    var _publish = function(topic, message) {
+        var message = message || {};
+
+        if (!topics[topic] || !topics[topic].queue.length) {
+            return;
+        }
+
+        topics[topic].queue.forEach(function(listener) {
+            listener(message);
+        });
+    }
+
+    window.events = {
+        subscribe: _subscribe,
+        unsubscribe: _unsubscribe,
+        publish: _publish,
+        topics: topics
+    };
+})();
+
+'use strict';
+
 (function(window, angular, events, undefined) {
     angular.module('sis', [
         'ngResource',
