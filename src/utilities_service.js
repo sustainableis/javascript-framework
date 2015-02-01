cfca7d3f5b7ec8924009a9d0487dd5983f4b3563
Dan Bozaru <bozarul@gmail.com>
2015-02-01 23:28:49 +0200

    add utilities service

---
+++ src/utilities_service.js
@@ -0,0 +1,26 @@
+'use strict';
+
+(function(angular) {
+  /**
+   * Resource for retrieving Utilities
+   *
+   * @param {string} controller
+   * @param {number|string} id
+   * @param {string} verb
+   * @param {string} verb_id
+   *
+   * Endpoints example:
+   *  - /v1/utilities/accounts
+   *  - /v1/utilities/statements/2/tree
+   *  - /v1/utilities/statements/2/tree/63
+   */
+  angular.module('sis.api').factory('UtilitiesService', function($resource,
+    url, version) {
+    return $resource(url + version + '/utilities/:controller/:id/:verb/:verb_id', {
+      controller: '@controller',
+      id: '@id',
+      verb: '@verb',
+      verb_id: '@verb_id'
+    });
+  });
+})(window.angular);
