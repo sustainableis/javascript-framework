8e35f64ecf1641d646c3df021550e7bcaf98e14d
Ionut Moraru <morion4000@gmail.com>
2015-02-10 14:58:06 +0200

    Implemented LayoutsService

---
+++ src/layouts_service.js
@@ -0,0 +1,21 @@
+(function(angular) {
+  /**
+   * Resource for retrieving Layouts
+   *
+   * @param {number|string} id
+   * @param {string} controller
+   * @param {string} verb
+   *
+   * Endpoints example:
+   *  - /v1/layouts
+   *  - /v1/layouts/1
+   */
+  angular.module('sis.api').factory('LayoutsService', function($resource,
+    url, version) {
+    return $resource(url + version + '/layouts/:id/:controller/:verb', {
+      id: '@id',
+      controller: '@controller',
+      verb: '@verb'
+    });
+  });
+})(window.angular);
