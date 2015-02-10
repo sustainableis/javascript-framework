e1d6dbedb1fbd15259230c3aa5180256dafbc9f5
Ionut Moraru <morion4000@gmail.com>
2015-02-10 18:14:25 +0200

    Implemented functionality to add modules to the dynamic view placeholders

--- src/views_provider.js
+++ src/views_provider.js
@@ -3,7 +3,7 @@
    * Provider for orchestrating the modules inserted on the page
    */
   angular.module('sis.modules').provider('sisViews', function() {
-    this.$get = function($injector, $q, $log, FacilitiesService, LayoutsService, path) {
+    this.$get = function($injector, $q, $log, $compile, FacilitiesService, LayoutsService, ViewsService, path) {
       var _this = this;
 
       /**
@@ -26,8 +26,32 @@
 
             options.scope.tpl = path + layout.slug + '/' + layout.slug + '.html';
 
-            options.scope.$on('$includeContentLoaded', function(){
-              callback();
+            options.scope.$on('$includeContentLoaded', function() {
+              var placeholders = $('.placeholder');
+
+              ViewsService.query({
+                id: view.id,
+                controller: 'modules'
+              }, function(modules) {
+                $log.debug(modules);
+
+                _.each(placeholders, function(placeholder) {
+                  var module = _.findWhere(modules, {placeholder: placeholder.id});
+
+                  if (module) {
+                    var script_tag = angular.element('<script>').attr({
+                                        type: 'text/javascript',
+                                        src: path + module.slug + '/' + module.slug + '.js'
+                                      });
+                    var module_tag = $compile('<' + module.slug + ' class="module" data-id="' + module.id + '">')(options.scope);
+
+                    angular.element(placeholder).append(module_tag);
+                    //angular.element('body').append(script_tag);
+                  }
+                });
+
+                callback();
+              });
             });
           });
         });
