8b6d649e5a995fa318c555184b5218327acb0d42
Ionut Moraru <morion4000@gmail.com>
2015-03-03 13:42:39 +0200

    Added jshint task as a build step
    changed the indentation for gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -1,44 +1,63 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        ngAnnotate: {
-            options: {
-                singleQuotes: true
-            },
-            dist: {
-                files: [{
-                    expand: true,
-                    src: 'src/**.js',
-                    dest: '.tmp/concat'
-                }]
-            }
-        },
-        concat: {
-            dist: {
-                src: [
-                    '.tmp/concat/src/framework.js',
-                    '.tmp/concat/src/**(!framework).js'
-                ],
-                dest: 'dist/<%= pkg.name %>.js',
-            }
-        },
-        uglify: {
-            options: {
-                mangle: true // Don't change variable and function names
-            },
-            dist: {
-                files: {
-                    'dist/<%= pkg.name %>.min.js': [
-                        'dist/<%= pkg.name %>.js',
-                    ]
-                }
-            }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      options: {
+        curly: true,
+        //eqeqeq: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          angular: true,
+          events: true,
+          _: true,
+          console: true
         }
-    });
+      },
+      dist: ['src/*']
+    },
+    ngAnnotate: {
+      options: {
+        singleQuotes: true
+      },
+      dist: {
+        files: [{
+          expand: true,
+          src: 'src/**.js',
+          dest: '.tmp/concat'
+        }]
+      }
+    },
+    concat: {
+      dist: {
+        src: [
+          '.tmp/concat/src/framework.js',
+          '.tmp/concat/src/**(!framework).js'
+        ],
+        dest: 'dist/<%= pkg.name %>.js',
+      }
+    },
+    uglify: {
+      options: {
+        mangle: true
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['dist/<%= pkg.name %>.js']
+        }
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-ng-annotate');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-ng-annotate');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
-    grunt.registerTask('default', ['ngAnnotate:dist', 'concat:dist', 'uglify:dist']);
+  grunt.registerTask('default', [
+    'jshint:dist',
+    'ngAnnotate:dist',
+    'concat:dist',
+    'uglify:dist'
+  ]);
 }
