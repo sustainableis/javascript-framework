8ad2c95cb9c5caf3b4e95381d0c432582622933e
Ionut Moraru <morion4000@gmail.com>
2015-03-14 14:27:42 +0200

    Updated the buildings resource to support more advanced endpoints

--- src/buildings_service.js
+++ src/buildings_service.js
@@ -12,13 +12,16 @@
    *  - /v1/buildings/1/outputs
    *  - /v1/buildings/1/outputs?category=demand_usage
    *  - /v1/buildings?facility_id=1
+   *  - /v1/buildings/1/mapping/areas
+   *  - /v1/buildings/1/outputs/mapping/geometry
    */
   angular.module('sis.api').factory('BuildingsService', function($resource,
     url, version) {
-    return $resource(url + version + '/buildings/:id/:controller/:verb', {
+    return $resource(url + version + '/buildings/:id/:controller/:verb/:action', {
       id: '@id',
       controller: '@controller',
-      verb: '@verb'
+      verb: '@verb',
+      action: '@action'
     }, {
       'update': {
         method: 'PUT'
