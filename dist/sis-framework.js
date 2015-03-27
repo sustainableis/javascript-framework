38a22265b3fafc6cd79beca32e924adcd909ee69
Ionut Moraru <morion4000@gmail.com>
2015-03-27 15:42:38 +0200

    Dist files

--- dist/sis-framework.js
+++ dist/sis-framework.js
@@ -184,6 +184,35 @@
   }]);
 })(window.angular);
 
+(function(angular) {
+  /**
+   * Resource for retrieving Configurations
+   *
+   * @param {number|string} id
+   * @param {string} controller
+   * @param {string} verb
+   * @param {string} action
+   *
+   * Endpoints example:
+   *  - /v1/configurations
+   *  - /v1/configurations/1
+   *  - /v1/configurations/1/values
+   */
+  angular.module('sis.api').factory('ConfigurationsService', ['$resource', 'url', 'version', function($resource,
+    url, version) {
+    return $resource(url + version + '/configurations/:id/:controller/:verb/:action', {
+      id: '@id',
+      controller: '@controller',
+      verb: '@verb',
+      action: '@action'
+    }, {
+      'update': {
+        method: 'PUT'
+      }
+    });
+  }]);
+})(window.angular);
+
   (function(angular, _) {
     /**
      * Provider for managing API calls
