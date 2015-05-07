92876fa62c1b18039d0fd3d90f50a85091b75db5
Ionut Moraru <morion4000@gmail.com>
2015-05-07 13:59:35 +0300

    Dist files

--- dist/sis-framework.js
+++ dist/sis-framework.js
@@ -558,6 +558,34 @@
 
 (function(angular) {
   /**
+   * Resource for retrieving Fields
+   *
+   * @param {number|string} id
+   * @param {string} controller
+   * @param {number|string} metric_name
+   * @param {string} verb
+   *
+   * Endpoints example:
+   *  - /v1/fields
+   *  - /v1/fields/1/metrics
+   *  - /v1/fields/1/metrics/:metric_name/data
+   */
+  angular.module('sis.api').factory('FieldsService', ['$resource', 'sisApi', function($resource, sisApi) {
+    return $resource(sisApi.url + sisApi.version + '/fields/:id/:controller/:metric_name/:verb', {
+      id: '@id',
+      controller: '@controller',
+      metric_name: '@metric_name',
+      verb: '@verb'
+    }, {
+      'update': {
+        method: 'PUT'
+      }
+    });
+  }]);
+})(window.angular);
+
+(function(angular) {
+  /**
    * Resource for retrieving Files
    *
    * @param {number|string} id
