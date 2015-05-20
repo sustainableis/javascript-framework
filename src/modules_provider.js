8112d5c7eaebf42e0f850f0a315b2e6cf15107e8
Ionut Moraru <morion4000@gmail.com>
2015-05-20 20:55:07 +0300

    Fixed a couple of bugs
    * reserved_channels_initialized is set to false when sisModules.destroy
    is called
    * modules data is loaded properly from cache

--- src/modules_provider.js
+++ src/modules_provider.js
@@ -120,14 +120,18 @@
               path + '.css'
             ]
           }).then(function() {
-            $compile(module)($rootScope);
-
             _send_channels_data(id);
           }, function() {
             $log.error('Module', tag, ':', id, '@', version, 'is missing files');
           });
 
           requests.push(request);
+
+          $rootScope.$on('ocLazyLoad.fileLoaded', function(e, file) {
+            if (file === path + '.js') {
+              $compile(module)($rootScope);
+            }
+          });
         });
 
         $q.all(requests).then(callback);
@@ -183,6 +187,8 @@
           clearing all the events listeners and resetting the modules list might
           cause issues. The destroy event is triggered when a view is destroyed.
          */
+
+        reserved_channels_initialized = false;
 
         // Remove all events listeners
         events.purge();
