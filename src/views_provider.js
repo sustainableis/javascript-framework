8c15b8795841be5cdceaa5d5c912936bdcfaf29b
Ionut Moraru <morion4000@gmail.com>
2015-05-21 22:15:31 +0300

    Fixed a bug with toggling views

--- src/views_provider.js
+++ src/views_provider.js
@@ -3,7 +3,7 @@
    * Provider for orchestrating the views inserted on the page
    */
   angular.module('sis.modules').provider('sisViews', function() {
-    this.$get = function($log, $compile, $rootScope, $ocLazyLoad, dataStore,
+    this.$get = function($log, $compile, $ocLazyLoad, dataStore,
       sisModules) {
       var _this = this;
 
@@ -11,7 +11,6 @@
        * Search for the view
        */
       var _load = function(options, callback) {
-        // TODO: Check if view for facilities, buildings, or organizations
         dataStore.get(options.topic,
           function(views, error) {
             // TODO: Make sure the first view is the right one
@@ -22,7 +21,7 @@
                 var path = sisModules.path + '/dist/' + layout.slug + '/' +
                   layout.version + '/' + layout.slug + '.min';
 
-                $rootScope.tpl = path + '.html';
+                options.scope.tpl = path + '.html';
 
                 $ocLazyLoad.load({
                   files: [
@@ -30,7 +29,7 @@
                   ]
                 });
 
-                $rootScope.$on('$includeContentLoaded', function() {
+                options.scope.$on('$includeContentLoaded', function() {
                   dataStore.get('service:views/id:' + view.id + '/controller:modules',
                     function(modules, error) {
                       var placeholders = $('.placeholder');
@@ -44,7 +43,7 @@
                           var module_markup = '<' + module.slug + ' class="module" data-id="' +
                             module.id + '" data-version="' + module.version + '">';
 
-                          module_element = $compile(module_markup)($rootScope);
+                          module_element = $compile(module_markup)(options.scope);
 
                           angular.element(placeholder).empty();
                           angular.element(placeholder).append(module_element);
