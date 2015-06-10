f43fdb53d95a69c9bdfaee0199aed543fd44d86d
Ionut Moraru <morion4000@gmail.com>
2015-06-10 14:59:34 +0300

    Dist files

--- dist/sis-framework.js
+++ dist/sis-framework.js
@@ -1125,64 +1125,76 @@
    * Provider for orchestrating the views inserted on the page
    */
   angular.module('sis.modules').provider('sisViews', function() {
-    this.$get = ['$log', '$compile', '$ocLazyLoad', 'dataStore', 'sisModules', function($log, $compile, $ocLazyLoad, dataStore,
+    this.$get = ['$log', '$compile', '$ocLazyLoad', '$q', '$rootScope', 'dataStore', 'sisModules', function($log, $compile, $ocLazyLoad, $q, $rootScope, dataStore,
       sisModules) {
       var _this = this;
 
       /**
-       * Search for the view
+       * Builds an internal list with modules embedded on the page and loads
+       * script files
        */
-      var _load = function(options, callback) {
-        dataStore.get(options.topic,
-          function(views, error) {
-            // TODO: Make sure the first view is the right one
-            var view = views[0];
+      var _init = function(callback) {
+        var views = angular.element('.view'),
+          requests = [];
 
-            dataStore.get('@service:layouts/id:' + view.layout_id,
-              function(layout, error) {
-                var path = sisModules.path + '/dist/' + layout.slug + '/' +
-                  layout.version + '/' + layout.slug + '.min';
+        _.each(views, function(view) {
+          var id = angular.element(view).data('id'),
+            version = angular.element(view).data('version'),
+            tag = angular.element(view).prop('tagName').toLowerCase(),
+            path = sisModules.path + '/dist/' + tag + '/' + version + '/' + tag + '.min',
+            request = $q.defer();
 
-                options.scope.tpl = path + '.html';
+          $ocLazyLoad.load({
+            serie: true,
+            files: [
+              // Preload the html template
+              path + '.html',
+              path + '.js',
+              path + '.css'
+            ]
+          }).then(function() {}, function() {
+            $log.error('Module', tag, ':', id, '@', version, 'is missing files');
+          });
 
-                $ocLazyLoad.load({
-                  files: [
-                    path + '.css'
-                  ]
+          dataStore.get('service:views/id:' + id + '/controller:modules',
+            function(modules, error) {
+              var placeholders = $(tag + ' .placeholder');
+
+              _.each(placeholders, function(placeholder) {
+                var module = _.findWhere(modules, {
+                  placeholder: placeholder.id
                 });
 
-                options.scope.$on('$includeContentLoaded', function() {
-                  dataStore.get('service:views/id:' + view.id + '/controller:modules',
-                    function(modules, error) {
-                      var placeholders = $('.placeholder');
+                if (module) {
+                  var module_markup = '<' + module.slug + ' class="module" data-id="' +
+                    module.id + '" data-version="' + module.version + '">';
 
-                      _.each(placeholders, function(placeholder) {
-                        var module = _.findWhere(modules, {
-                          placeholder: placeholder.id
-                        });
+                  module_element = $compile(module_markup)($rootScope);
 
-                        if (module) {
-                          var module_markup = '<' + module.slug + ' class="module" data-id="' +
-                            module.id + '" data-version="' + module.version + '">';
-
-                          module_element = $compile(module_markup)(options.scope);
-
-                          angular.element(placeholder).empty();
-                          angular.element(placeholder).append(module_element);
-                        }
-                      });
-
-                      callback();
-                    }
-                  );
-                });
+                  angular.element(placeholder).empty();
+                  angular.element(placeholder).append(module_element);
+                }
               });
-          }
-        );
+
+              request.resolve();
+            },
+            false
+          );
+
+          $rootScope.$on('ocLazyLoad.fileLoaded', function(e, file) {
+            if (file === path + '.js') {
+              $compile(view)($rootScope);
+            }
+          });
+
+          requests.push(request.promise);
+        });
+
+        $q.all(requests).then(callback);
       };
 
       return {
-        load: _load,
+        init: _init
       };
     }];
   });
