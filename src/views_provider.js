b7d1de3b1856b66de45980219032cfba95110a04
Ionut Moraru <morion4000@gmail.com>
2015-06-15 14:18:39 +0300

    Fix for views not loading all the time

--- src/views_provider.js
+++ src/views_provider.js
@@ -30,34 +30,33 @@
               path + '.js',
               path + '.css'
             ]
-          }).then(function() {}, function() {
-            $log.error('Module', tag, ':', id, '@', version, 'is missing files');
-          });
+          }).then(function() {
+            dataStore.get('service:views/id:' + id + '/controller:modules',
+              function(modules, error) {
+                var placeholders = $(tag + ' .placeholder');
 
-          dataStore.get('service:views/id:' + id + '/controller:modules',
-            function(modules, error) {
-              var placeholders = $(tag + ' .placeholder');
+                _.each(placeholders, function(placeholder) {
+                  var module = _.findWhere(modules, {
+                    placeholder: placeholder.id
+                  });
 
-              _.each(placeholders, function(placeholder) {
-                var module = _.findWhere(modules, {
-                  placeholder: placeholder.id
+                  if (module) {
+                    var module_markup = '<' + module.slug + ' class="module" data-id="' +
+                      module.id + '" data-version="' + module.version + '">';
+
+                    module_element = $compile(module_markup)($rootScope);
+
+                    angular.element(placeholder).empty();
+                    angular.element(placeholder).append(module_element);
+                  }
                 });
 
-                if (module) {
-                  var module_markup = '<' + module.slug + ' class="module" data-id="' +
-                    module.id + '" data-version="' + module.version + '">';
-
-                  module_element = $compile(module_markup)($rootScope);
-
-                  angular.element(placeholder).empty();
-                  angular.element(placeholder).append(module_element);
-                }
-              });
-
-              request.resolve();
-            },
-            false
-          );
+                request.resolve();
+              }
+            );
+          }, function() {
+            $log.error('Module', tag, ':', id, '@', version, 'is missing files');
+          });
 
           $rootScope.$on('ocLazyLoad.fileLoaded', function(e, file) {
             if (file === path + '.js') {
