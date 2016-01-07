761b8ef1cfb45bc452e356639259a04270e45525
Ionut Moraru <morion4000@gmail.com>
2016-01-07 17:42:27 +0200

    Removed an extra param for emails service

--- src/emails_service.js
+++ src/emails_service.js
@@ -5,7 +5,6 @@
    * @param {number|string} id
    * @param {string} controller
    * @param {string} verb
-   * @param {number|string} verb_id
    * @param {string} action
    *
    * Endpoints example:
@@ -15,11 +14,10 @@
    *  - /v1/emails/subscriptions/:subscription_id/trigger
    */
   angular.module('sis.api').factory('ViewsService', function($resource, sisApi) {
-    return $resource(sisApi.url + sisApi.version + '/views/:id/:controller/:verb/:action', {
+    return $resource(sisApi.url + sisApi.version + '/emails/:id/:controller/:verb/:action', {
       id: '@id',
       controller: '@controller',
       verb: '@verb',
-      verb_id: '@verb_id',
       action: '@action'
     }, {
       'update': {
