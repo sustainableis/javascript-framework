4e773087f6a4b91dd37e8cba9048051f889af7cb
Ionut Moraru <morion4000@gmail.com>
2016-01-20 10:49:21 +0200

    Added services to sisApi provider

--- src/api_provider.js
+++ src/api_provider.js
@@ -5,11 +5,13 @@
   angular.module('sis.api').provider('sisApi', function() {
     this.url = 'http://api.sustainableis.com/';
     this.version = 'v1';
+    this.services = {};
 
     this.$get = function() {
       return {
         url: this.url,
-        version: this.version
+        version: this.version,
+        services: this.services
       };
     };
   });
