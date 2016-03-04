2aaabf3831068cb3a5b5e28cf1c5a753ef32b43a
razvanbp <razvanbp@gmail.com>
2016-03-04 18:10:44 +0200

    Added StacksService min

--- dist/sis-framework.js
+++ dist/sis-framework.js
@@ -1062,6 +1062,33 @@
 
 (function(angular) {
   /**
+   * Resource for retrieving Stacks
+   *
+   * @param {number|string} id
+   * @param {string} controller
+   * @param {string} verb
+   * @param {string} action
+   *
+   * Endpoints example:
+   *  - /v1/stacks
+   *  - /v1/stacks/1
+   *  - /v1/stacks/1/endpoints
+   */
+  angular.module('sis.api').factory('StacksService', ['$resource', 'sisApi', function($resource, sisApi) {
+    return $resource(sisApi.url + sisApi.version + '/stacks/:id/:controller/:verb', {
+      id: '@id',
+      controller: '@controller',
+      verb: '@verb'
+    }, {
+      'update': {
+        method: 'PUT'
+      }
+    });
+  }]);
+})(window.angular);
+
+(function(angular) {
+  /**
    * Resource for retrieving Users
    *
    * @param {number|string} id
